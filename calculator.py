406b45937e1b0a86787f0d8c9fdb48b69669f043
Cori Lint <corilint@gmail.com>
2021-01-06 20:23:12 -0600

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+
+# Replace this with your code
+# given user input, turn input into list using split
+# if the first item in the list is "+"
+    #check if length of list is 3
+        #if yes
+            #turn 2nd and 3rd items to integers
+            #call add function with integers
+
+        #if no
+            #error (make them input again)
